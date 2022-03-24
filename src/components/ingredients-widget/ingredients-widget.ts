3bf74e027fc09e71a9a488cf04f6ff0f42945082
Anton Gustafsson <ajacobsson98@gmail.com>
2022-03-24 20:30:02 +0100

    add tiny-swiper

--- src/components/ingredients-widget/ingredients-widget.ts
+++ src/components/ingredients-widget/ingredients-widget.ts
@@ -1,22 +1,37 @@
 import { getCocktails } from 'functions/cocktail-functions';
-import Swipe from 'swipejs';
+// import Swipe from 'swipejs';
+import Swiper from 'tiny-swiper/lib/index.full.js';
 
 export class IngredientsWidget {
     public position = 1;
     public cocktails: any[] = [];
+    public swipeElement: HTMLElement;
+    public swiper;
 
     bind() {
         this.cocktails = getCocktails();
     }
 
     attached() {
-        (window as any).mySwipe = new Swipe(document.getElementById('slider'), {
-            continuous: false,
-            callback: (index, elem, dir) => {
-                this.position = index + 1;
+        this.swiper = new Swiper(this.swipeElement, {
+            lazyload: {
+                loadPrevNext: false,
+                loadPrevNextAmount: 1,
+                loadOnTransitionStart: false,
+                elementClass: 'swiper-lazy',
+                loadingClass: 'swiper-lazy-loading',
+                loadedClass: 'swiper-lazy-loaded',
+                preloaderClass: 'swiper-lazy-preloader',
             },
-            disableScroll: true,
-            stopPropagation: true,
+            touchMoveStopPropagation: true,
         });
+
+        this.swiper.on('after-slide', newIndex => {
+            this.position = newIndex + 1;
+        });
+    }
+
+    detached() {
+        this.swiper.destroy();
     }
 }
