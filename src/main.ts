70be42fb00852d2f503729bebbc20038f2c76d44
Anton Gustafsson <ajacobsson98@gmail.com>
2022-04-02 08:19:11 +0200

    more styling

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ export function configure(aurelia: Aurelia): void {
     aurelia.use
         .standardConfiguration()
         .developmentLogging(PRODUCTION ? 'none' : 'debug')
-        .feature(PLATFORM.moduleName('components/index'));
+        .feature(PLATFORM.moduleName('components/index'))
+        .plugin(PLATFORM.moduleName('aurelia-animator-css'));
 
     aurelia.use.aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('app')));
 }
