e48e3fec92b4486114ad9a1be2351355975934fd
Anton Gustafsson <ajacobsson98@gmail.com>
2022-06-13 22:15:01 +0200

    feat(all): add alphabeticalGroup

---
+++ src/models/alphabetical-group.ts
@@ -0,0 +1,20 @@
+export class AlphabeticalGroup<T> {
+    constructor(public letter: string, public items: T[]) {}
+}
+
+export function ToAlphabeticalGroup<T>(items: T[], property = 'name'): AlphabeticalGroup<T>[] {
+    const letters = [...new Set(items.map(x => x[property]?.charAt(0)?.toLowerCase()).filter(x => x !== undefined))];
+
+    const group: AlphabeticalGroup<T>[] = [];
+
+    letters.forEach(element => {
+        group.push(
+            new AlphabeticalGroup(
+                element,
+                items.filter(x => x[property]?.charAt(0)?.toLowerCase() === element)
+            )
+        );
+    });
+
+    return group.sort((a, b) => a.letter.localeCompare(b.letter));
+}
