515abb6acc41ea23f272e5ec04ca5d54d0b2a5f5
Anton Gustafsson <anton@moimob.com>
2022-06-13 22:22:26 +0200

    feat(all): add alphabeticalGroup (#38)

--- src/modules/ingredients/ingredients.ts
+++ src/modules/ingredients/ingredients.ts
@@ -2,17 +2,21 @@ import { getManageIngredientModels } from 'functions/ingredient-functions';
 import { ManageIngredientModel } from 'models/ingredient';
 import { LocalStorageService } from 'services/local-storage-service';
 import { inject } from 'aurelia-framework';
+import { AlphabeticalGroup, ToAlphabeticalGroup } from 'models/alphabetical-group';
 
 @inject(LocalStorageService)
 export class Ingredients {
-    public ingredients: ManageIngredientModel[] = [];
+    public ingredients: AlphabeticalGroup<ManageIngredientModel>[] = [];
     public activeIngredientIds: number[] = [];
 
     constructor(private _localStorageService: LocalStorageService) {}
 
     activate() {
         this.activeIngredientIds = this._localStorageService.getIngredientIds();
-        this.ingredients = getManageIngredientModels(this.activeIngredientIds);
+
+        const ingredientModels = getManageIngredientModels(this.activeIngredientIds);
+
+        this.ingredients = ToAlphabeticalGroup<ManageIngredientModel>(ingredientModels);
     }
 
     public toggleIngredient(ingredientModel: ManageIngredientModel) {
