2ff4a9cf897c6ac9bd996854cbf22f9a73c5f01b
Anton Gustafsson <ajacobsson98@gmail.com>
2022-06-27 21:06:33 +0200

    feat(all): add local font

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
     appId: 'com.moimob.cocktail',
-    appName: 'CocktailApp',
+    appName: 'Drinkable',
     webDir: 'www',
     bundledWebRuntime: false,
     backgroundColor: '#161212',
