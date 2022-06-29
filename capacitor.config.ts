6ab2e3a8fa6992f830758aaaf3d43ffe397d4fe1
Anton Gustafsson <ajacobsson98@gmail.com>
2022-06-29 22:03:19 +0200

    fix(settings): scroll issue

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-    appId: 'com.moimob.cocktail',
+    appId: 'com.moimob.drinkable',
     appName: 'Drinkable',
     webDir: 'www',
     bundledWebRuntime: false,
