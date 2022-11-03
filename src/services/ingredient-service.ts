82630d2a08e9f6a390b2420f1e98c7e707fb6480
Anton Gustafsson <anton@moimob.com>
2022-11-03 20:20:33 +0100

    feat(all): ingredient translations (#145)
    * feat(all): ingredient translations
    
    * update README.md

--- src/services/ingredient-service.ts
+++ src/services/ingredient-service.ts
@@ -1,18 +1,29 @@
 import { inject } from 'aurelia-framework';
+import { I18N } from 'aurelia-i18n';
 import { Cocktail, ExtendedIngredientGroup, IngredientGroup } from 'domain/entities/cocktail';
 import { CreatedIngredientModel, Ingredient, ManageIngredientModel } from 'domain/entities/ingredient';
 import { SpiritType } from 'domain/enums/spirit-type';
 import { getStaticIngredients } from 'functions/ingredient-functions';
 import { LocalStorageService } from './local-storage-service';
 
-@inject(LocalStorageService)
+@inject(LocalStorageService, I18N)
 export class IngredientService {
-    private _ingredients: Ingredient[] = getStaticIngredients();
+    private _ingredients: Ingredient[] = [];
     private _createdIngredients: Ingredient[] = [];
     private _highestId = 0;
 
-    constructor(private _localStorageService: LocalStorageService) {
+    constructor(private _localStorageService: LocalStorageService, private _i18n: I18N) {
         this._createdIngredients = this._localStorageService.getIngredients();
+
+        const staticIngredients = getStaticIngredients();
+        staticIngredients.forEach(element => {
+            this._ingredients.push({
+                id: element.id,
+                name: this._i18n.tr(element.translation, { ns: 'ingredients' }),
+                spiritType: element.spiritType,
+                translation: element.translation,
+            });
+        });
 
         this._createdIngredients.forEach(x => {
             const id = Number(x.id.split('-')[1]);
@@ -41,6 +52,7 @@ export class IngredientService {
             id: x.id,
             name: x.name,
             spiritType: x.spiritType,
+            translation: x.translation,
             isActive: false,
         }));
     }
@@ -69,6 +81,7 @@ export class IngredientService {
             id: x.id,
             name: x.name,
             spiritType: x.spiritType,
+            translation: x.translation,
             usedInCocktailNames: cocktails
                 .filter(y => y.ingredientGroups.some(z => z.ingredientId === x.id))
                 .map(a => a.name),
@@ -80,6 +93,7 @@ export class IngredientService {
             id: x.id,
             name: x.name,
             spiritType: x.spiritType,
+            translation: x.translation,
             isActive: activeIds.includes(x.id),
         }));
     }
@@ -104,6 +118,14 @@ export class IngredientService {
 
         this._ingredients = this._ingredients.filter(x => x.id !== ingredient.id);
         this._ingredients.push(ingredient);
+    }
+
+    public updateTranslation() {
+        this._ingredients.forEach(element => {
+            if (element.translation !== undefined) {
+                element.name = this._i18n.tr(element.translation, { ns: 'ingredients' });
+            }
+        });
     }
 
     public async deleteIngredient(id: string) {
