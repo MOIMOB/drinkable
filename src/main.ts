935cd8ffb5978de3027627c1c18b5f913d1b9b40
Anton Gustafsson <anton@moimob.com>
2022-12-07 21:06:20 +0100

    feat(all): add aurelia-hotjar (#183)

--- src/main.ts
+++ src/main.ts
@@ -7,42 +7,48 @@ import 'toastify-js/src/toastify.css';
 import { LocalStorageService } from 'services/local-storage-service';
 import { I18N, TCustomAttribute } from 'aurelia-i18n';
 import HttpApi from 'i18next-http-backend';
+import { HotjarConfig } from 'aurelia-hotjar';
 
 export async function configure(aurelia: Aurelia): Promise<void> {
+    const localStorageService = aurelia.container.get(LocalStorageService);
+    await localStorageService.initialize();
+
+    const language = localStorageService.getSettings().language ?? 'en';
+
     aurelia.use
         .standardConfiguration()
         .developmentLogging(PRODUCTION ? 'none' : 'debug')
         .feature(PLATFORM.moduleName('components/index'))
         .plugin(PLATFORM.moduleName('aurelia-animator-css'))
         .plugin(PLATFORM.moduleName('aurelia-validation'))
+        .plugin(PLATFORM.moduleName('aurelia-hotjar'), (config: HotjarConfig) => {
+            config.id = 3079806;
+            config.snippetVersion = 6;
+            config.disabled = !WEB;
+        })
         .plugin(PLATFORM.moduleName('aurelia-dialog'), (config: DialogConfiguration) => {
             config.useDefaults();
             config.useCSS('');
+        })
+        .plugin(PLATFORM.moduleName('aurelia-i18n'), (instance: I18N) => {
+            const aliases = ['t', 'i18n'];
+            TCustomAttribute.configureAliases(aliases);
+
+            instance.i18next.use(HttpApi);
+
+            return instance.setup({
+                backend: {
+                    loadPath: './locales/{{lng}}/{{ns}}.json'
+                },
+                attributes: aliases,
+                lng: language,
+                fallbackLng: 'en',
+                debug: false,
+                ns: ['ingredients', 'translation'],
+                defaultNS: 'translation'
+            });
         });
 
-    const localStorageService = aurelia.container.get(LocalStorageService);
-    await localStorageService.initialize();
-
-    const language = localStorageService.getSettings().language ?? 'en';
-
-    aurelia.use.plugin(PLATFORM.moduleName('aurelia-i18n'), (instance: I18N) => {
-        const aliases = ['t', 'i18n'];
-        TCustomAttribute.configureAliases(aliases);
-
-        instance.i18next.use(HttpApi);
-
-        return instance.setup({
-            backend: {
-                loadPath: './locales/{{lng}}/{{ns}}.json'
-            },
-            attributes: aliases,
-            lng: language,
-            fallbackLng: 'en',
-            debug: false,
-            ns: ['ingredients', 'translation'],
-            defaultNS: 'translation'
-        });
-    });
-
-    aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('app')));
+    await aurelia.start();
+    await aurelia.setRoot(PLATFORM.moduleName('app'));
 }
