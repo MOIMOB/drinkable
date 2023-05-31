b03e9be43738e8e1cc284ba92f7424e7f17f4551
KHJcode <kimstudswcode4485@gmail.com>
2023-06-01 00:26:01 +0900

    feat: add korea language

--- src/data/languages.ts
+++ src/data/languages.ts
@@ -14,5 +14,6 @@ const languages = [
     { value: 'ru', name: 'Русский' },
     { value: 'pl', name: 'Polski' },
     { value: 'pt', name: 'Português' },
-    { value: 'si', name: 'සිංහල (sĩhala)' }
+    { value: 'si', name: 'සිංහල (sĩhala)' },
+    { value: 'kr', name: '한국어' }
 ];
