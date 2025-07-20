e4ce49d1f6fdcf26180c645c646023fa5b3dc2bc
Anton Gustafsson <anton@moimob.com>
2025-07-20 22:34:18 +0200

    Fix android overlapping (#582)
    * fix: android overlapping
    
    * fix: add changelog entry for app overlap issue with system GUI
    
    * fix: add changelog entry for app overlap issue with system GUI

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,10 @@ const config: CapacitorConfig = {
     appId: 'com.moimob.drinkable',
     appName: 'Drinkable',
     webDir: 'dist',
-    backgroundColor: '#161314'
+    backgroundColor: '#161314',
+    android: {
+        adjustMarginsForEdgeToEdge: 'auto'
+    }
 };
 
 export default config;
